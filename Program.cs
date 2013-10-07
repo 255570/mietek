23c01026c6f0202465552f1e2acd734ed606eab0
255570 <mateusz14155@mat.umk.pl>
2013-10-07 11:38:29 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace program1
+{
+    class program1
+    {
+        static void Main()
+        {
+            Console.WriteLine("255570");
+
+            
+        
+            Console.ReadKey();
+        }
+    }
+}

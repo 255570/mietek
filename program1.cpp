ad0726d624e4c300b889c38fede0bcb7a86d5823
255570 <mateusz14155@mat.umk.pl>
2013-10-07 11:29:57 +0200

    first commit

---
+++ program1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+	cout<<"255570";
+	
+	return 0;
+}
